4d424df4a70be864a8976e55131cbce832d99f8e
dafeys <dafeys@outlook.com>
2023-04-09 00:55:21 +0300

    4 module strings

---
+++ 04_strings/4_strings.rb
@@ -0,0 +1,24 @@
+ name = String.new("Name")
+ p name
+
+# multiline strings
+words = <<MLS
+   some text here
+            and here
+This will be a multi-line strings
+
+--- Goodbye! ---
+MLS
+
+p words
+puts
+puts words
+
+
+# escape characters
+puts "Juliet said 'Goodbye' to Romeo"
+puts 'Juliey said "Goodbye" to Romeo'
+
+puts "Juliet daid \"Goodbye\" to Romeo"
+
+

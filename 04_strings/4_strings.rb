90c2cc15855474c5f210c4d02464aeb70361d09e
dafeys <dafeys@outlook.com>
2023-04-09 14:41:27 +0300

    try vscode remote

--- 04_strings/4_strings.rb
+++ 04_strings/4_strings.rb
@@ -21,4 +21,38 @@ puts 'Juliey said "Goodbye" to Romeo'
 
 puts "Juliet daid \"Goodbye\" to Romeo"
 
+# single vs double quoted quotes
+str = '!'
+puts "Hello\nworld#{str}"
+puts 'Hello\nworld#{str}'
+
+# equality
+a = "Hello"
+b = "hello"
+c = "Hello "
+
+p b == a
+p "Apple" < "Banana"    #true
+p "hello" < "help"      #true
+p "A" < "a"             #true
+p "X" < "a"             #true
+p "Help" < "banana"     #true
+
+# concatenate
+first_name = "Harry "
+last_name = "Potter"
+
+p first_name + last_name
+
+full_name = first_name # link!!!
+p full_name.concat(last_name) # modify orig string
+p ("first = #{first_name}")
+p ("last = #{last_name}")
+puts
+full_name = ""
+p full_name << last_name << " Wizard"
+
+p last_name.prepend(first_name) # modify orig string
+
+
 

9ae0917ad6b9efa983c22e5617cbf28f33ebb564
dafeys <dafeys@outlook.com>
2023-05-01 02:18:05 +0300

    modules

---
+++ 19_modules/19_modules.rb
@@ -0,0 +1,21 @@
+module LengthConversions
+  WEBSITE = 'https://moesko.com'
+
+  def self.miles_to_feet(miles)
+    miles * 5280
+  end
+
+  def self.miles_to_inches(miles)
+    feet = miles_to_feet(miles)
+    feet * 12
+  end
+
+  def self.miles_to_centimeters(miles)
+    inches = miles_to_inches(miles)
+  inches * 2.544
+  end
+end
+
+puts LengthConversions::WEBSITE
+puts LengthConversions.miles_to_feet(100)
+puts LengthConversions.miles_to_inches(200)
